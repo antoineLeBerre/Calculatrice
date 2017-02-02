471049a5f3d91170fb159be2449b825fd8fbed1a
lapinoub <ant.le.berre@gmail.com>
2017-02-02 22:11:43 +0100

    Passage portable a bureau

---
+++ app/src/main/java/com/calculatrice/alb/calculatrice/MainActivity.java
@@ -0,0 +1,226 @@
+package com.calculatrice.alb.calculatrice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView resultat;
+    Button b0;
+    Button b1;
+    Button b2;
+    Button b3;
+    Button b4;
+    Button b5;
+    Button b6;
+    Button b7;
+    Button b8;
+    Button b9;
+    Button bPlus;
+    Button bMoins;
+    Button bMultiplier;
+    Button bDiviser;
+    Button bPoint;
+    Button bClear;
+    Button bEgale;
+    String operande:
+    String operande1;
+    String operane2;
+    String resultatEnCoursText;
+    int resultatEnCours;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        resultat = (TextView)findViewById(R.id.textCalc);
+        b0 = (Button)findViewById(R.id.button0);
+        b1 = (Button)findViewById(R.id.button1);
+        b2 = (Button)findViewById(R.id.button2);
+        b3 = (Button)findViewById(R.id.button3);
+        b4 = (Button)findViewById(R.id.button4);
+        b5 = (Button)findViewById(R.id.button5);
+        b6 = (Button)findViewById(R.id.button6);
+        b7 = (Button)findViewById(R.id.button7);
+        b8 = (Button)findViewById(R.id.button8);
+        b9 = (Button)findViewById(R.id.button9);
+        bPlus = (Button)findViewById(R.id.buttonPlus);
+        bMoins = (Button)findViewById(R.id.buttonMoins);
+        bMultiplier = (Button)findViewById(R.id.buttonMultiplier);
+        bDiviser = (Button)findViewById(R.id.buttonDiviser);
+        bPoint = (Button)findViewById(R.id.buttonPoint);
+        bClear = (Button)findViewById(R.id.buttonClear);
+        bEgale = (Button)findViewById(R.id.buttonEgale);
+
+        b0.setOnClickListener(this);
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+        b3.setOnClickListener(this);
+        b4.setOnClickListener(this);
+        b5.setOnClickListener(this);
+        b6.setOnClickListener(this);
+        b7.setOnClickListener(this);
+        b8.setOnClickListener(this);
+        b9.setOnClickListener(this);
+        bPlus.setOnClickListener(this);
+        bMoins.setOnClickListener(this);
+        bMultiplier.setOnClickListener(this);
+        bDiviser.setOnClickListener(this);
+        bPoint.setOnClickListener(this);
+        bClear.setOnClickListener(this);
+        bEgale.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            /*
+            * Les boutons permettent d'afficher leurs contenus dans le TextView (exepté le bouton Clear)
+            * Les boutons de 0 à 9 et . si il y a un zero qui est le 1er le change par le chiffre sur lequel on clique
+            * Le bouton Clear effacera toute la ligne
+            * Le bouton egale nous mettrera le resultat final
+            * les boutons + - * / permettra de faire le calcul de 2 operandes, si on passe à une 2 ele ioeratuib fait a 1ere operation
+            */
+            case R.id.button0 :
+                operande = resultat.getText().toString();
+                if (operande.equals("0")){
+                    resultatEnCours = operande;
+                    resultat.setText(resultatEnCours);
+                }
+                else {
+                    resultatEnCours += b0.getText().toString();
+                    resultat.setText(resultatEnCours);
+                }
+                break;
+            case R.id.button1 :
+                operande = resultat.getText().toString();
+                if (operande.equals("0")){
+                    resultatEnCours = b1.getText().toString();
+                    resultat.setText(resultatEnCours);
+                }
+                else {
+                    resultatEnCours = resultatEnCours + b1.getText().toString();
+                    resultat.setText(resultatEnCours);
+                }
+                break;
+            case R.id.button2 :
+                operande = resultat.getText().toString();
+                if (operande.equals("0")){
+                    resultatEnCours = b2.getText().toString();
+                    resultat.setText(resultatEnCours);
+                }
+                else {
+                    resultatEnCours += b2.getText().toString();
+                    resultat.setText(resultatEnCours);
+                }
+                break;
+            case R.id.button3 :
+                operande = resultat.getText().toString();
+                if (operande.equals("0")){
+                    resultatEnCours = b3.getText().toString();
+                    resultat.setText(resultatEnCours);
+                }
+                else {
+                    resultatEnCours += b3.getText().toString();
+                    resultat.setText(resultatEnCours);
+                }
+                break;
+            case R.id.button4 :
+                operande = resultat.getText().toString();
+                if (operande.equals("0")){
+                    resultatEnCours = b4.getText().toString();
+                    resultat.setText(resultatEnCours);
+                }
+                else {
+                    resultatEnCours += b4.getText().toString();
+                    resultat.setText(resultatEnCours);
+                }
+                break;
+            case R.id.button5 :
+                operande = resultat.getText().toString();
+                if (operande.equals("0")){
+                    resultatEnCours = b5.getText().toString();
+                    resultat.setText(resultatEnCours);
+                }
+                else {
+                    resultatEnCours += b5.getText().toString();
+                    resultat.setText(resultatEnCours);
+                }
+                break;
+            case R.id.button6 :
+                operande = resultat.getText().toString();
+                if (operande.equals("0")){
+                    resultatEnCours = b6.getText().toString();
+                    resultat.setText(resultatEnCours);
+                }
+                else {
+                    resultatEnCours += b6.getText().toString();
+                    resultat.setText(resultatEnCours);
+                }
+                break;
+            case R.id.button7 :
+                operande = resultat.getText().toString();
+                if (operande.equals("0")){
+                    resultatEnCours = b7.getText().toString();
+                    resultat.setText(resultatEnCours);
+                }
+                else {
+                    resultatEnCours += b7.getText().toString();
+                    resultat.setText(resultatEnCours);
+                }
+                break;
+            case R.id.button8 :
+                operande = resultat.getText().toString();
+                if (operande.equals("0")){
+                    resultatEnCoursText = b8.getText().toString();
+                    resultat.setText(resultatEnCours);
+                }
+                else {
+                    resultatEnCours += b8.getText().toString();
+                    resultat.setText(resultatEnCours);
+                }
+                break;
+            case R.id.button9 :
+                operande = resultat.getText().toString();
+                if (operande.equals("0")){
+                    resultatEnCoursText = b9.getText().toString();
+                    resultat.setText(resultatEnCours);
+                }
+                else {
+                    resultatEnCoursText += b9.getText().toString();
+                    resultat.setText(resultatEnCoursText);
+                }
+                break;
+            case R.id.buttonPlus :
+                if (operande1 == null){
+                    operande1 = resultatEnCoursText:
+                    resultatEnCoursText = "0";
+                }
+                else {
+                    resultatEnCours = Integer.parseInt(resultatEnCoursText)
+                            + Integer.parseInt(operande1);
+                    resultat.setText(resultatEnCours);
+                }
+                break;
+            case R.id.buttonMoins :
+                break;
+            case R.id.buttonMultiplier :
+                break;
+            case R.id.buttonDiviser :
+                break;
+            case R.id.buttonPoint :
+                break;
+            case R.id.buttonClear :
+                break;
+            case R.id.buttonEgale :
+                break;
+        }
+
+    }
+}
